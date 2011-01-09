57b9dbcca5b58ca7fe4d8e2b6a1ae235868cbb0b
Diego <diego@fmpwizard.com>
2011-01-09 04:13:08 -0500

    removed extra jar files and fixed minor default value on injected val

--- src/main/scala/code/snippet/Overview.scala
+++ src/main/scala/code/snippet/Overview.scala
@@ -18,7 +18,7 @@ class Overview extends Logger with SimpleInjector{
 
   //val showingVersion= http.S.param("v") openOr("ERROR") 
   val showingVersion= new Inject[Box[String]]( (http.S.param("v")  ) ){}
-  showingVersion.default.set(Box("ERROR"))
+  showingVersion.default.set(http.S.param("v"))
 
   info(showingVersion.vend)
   def renderAgentResult( xhtml: NodeSeq ) = {
